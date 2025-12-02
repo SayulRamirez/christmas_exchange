ee2cd70cff7e6439e640c3879233aa305e12119d
SayulRamirez <sayulramirez@gmail.com>
2025-12-02 00:23:06 -0600

    fix: prevencion de ultimos tres participantes para que el ultimo no se elija a si mismo, y validacion de concurrencia al seleccionar al mismo tiempo a un solo participante, evitandolo y refrescando las selecciones de uno de los usuarios

--- src/main/java/com/saul/christmas_exchange/repository/ParticipantRepository.java
+++ src/main/java/com/saul/christmas_exchange/repository/ParticipantRepository.java
@@ -15,4 +15,6 @@ public interface ParticipantRepository extends JpaRepository<Participant, Intege
     Optional<Participant> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    int countBySelectedIsFalse(Integer giver);
 }
--- src/main/java/com/saul/christmas_exchange/service/ParticipantServiceImpl.java
+++ src/main/java/com/saul/christmas_exchange/service/ParticipantServiceImpl.java
@@ -5,11 +5,14 @@ import com.saul.christmas_exchange.entity.Participant;
 import com.saul.christmas_exchange.model.ParticipantResponse;
 import com.saul.christmas_exchange.repository.ExchangeRepository;
 import com.saul.christmas_exchange.repository.ParticipantRepository;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class ParticipantServiceImpl implements ParticipantService {
@@ -39,14 +42,37 @@ public class ParticipantServiceImpl implements ParticipantService {
         return toResponse(receiver);
     }
 
+    @Transactional
     @Override
     public ParticipantResponse selectedReceiver(int giver, int receiver) {
+
+        if (participantRepository.countBySelectedIsFalse(giver) == 2) {
+
+            Optional<Participant> participant = exchangeRepository.findReceiverByIdGiver(receiver);
+
+            if (participant.isPresent()) {
+                if (participant.get().getIdParticipant() == giver) {
+                    final int receiverSearch = receiver;
+
+                    Participant newReceiver = participantRepository.findAllBySelectedIsFalseAndIdParticipantIsNot(giver)
+                            .stream()
+                            .filter(p -> p.getIdParticipant() != receiverSearch)
+                            .findFirst()
+                            .orElseThrow();
+
+                    receiver = newReceiver.getIdParticipant();
+                }
+            }
+
+        }
 
         Participant giverEntity = participantRepository.findById(giver)
                 .orElseThrow(() -> new EntityNotFoundException("No se encontró al participante con id: " + giver));
 
         Participant receiverEntity = participantRepository.findById(receiver)
-                .orElseThrow(() -> new EntityNotFoundException("No se encontró al participante con id: " + receiver));
+                .orElseThrow(() -> new EntityNotFoundException("No se encontró al participante"));
+
+        if (receiverEntity.isSelected()) throw new EntityExistsException("Ya se selecciono");
 
         giverEntity.setAssigned(true);
 
